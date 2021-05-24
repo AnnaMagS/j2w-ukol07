f281392e3aaf058f208e72b634f3d7cef4c17b84
18-1855-1 <annamagdalena.stranska@unicorncollege.cz>
2021-05-24 23:46:59 +0200

    Zobrazuje se formular

--- src/main/java/cz/czechitas/java2webapps/ukol7/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol7/controller/VizitkaController.java
@@ -6,12 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -32,6 +32,23 @@ public class VizitkaController {
     public Object seznam() {
         return new ModelAndView("seznam")
                 .addObject("vizitky", repository.findAll());
+    }
+
+    @GetMapping("/nova")
+    public Object nova() {
+        return new ModelAndView("formular")
+                .addObject("vizitka", new Vizitka());
+    }
+
+    @PostMapping("/nova")
+    public Object pridat(@ModelAttribute("vizitka") @Valid Vizitka vizitka, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            System.out.println("chyba");
+            return "formular";
+        }
+        vizitka.setId(null);
+        repository.save(vizitka);
+        return "redirect:/";
     }
 
     @GetMapping("/{id:[0-9]+}")
