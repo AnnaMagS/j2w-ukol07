e89e1f442149e4994a75ec4b9a85d3201d7dff2e
18-1855-1 <annamagdalena.stranska@unicorncollege.cz>
2021-05-24 21:42:27 +0200

    Entity, repository and controller

---
+++ src/main/java/cz/czechitas/java2webapps/ukol7/controller/VizitkaController.java
@@ -0,0 +1,31 @@
+package cz.czechitas.java2webapps.ukol7.controller;
+
+import cz.czechitas.java2webapps.ukol7.repository.VizitkaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class VizitkaController {
+    private final VizitkaRepository repository;
+
+    @Autowired
+    public VizitkaController(VizitkaRepository repository) {
+        this.repository = repository;
+    }
+
+    @InitBinder
+    public void nullStringBinding(WebDataBinder binder) {
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+
+    @GetMapping("/")
+    public Object seznam() {
+        return new ModelAndView("seznam")
+                .addObject("vizitka", repository.findAll());
+    }
+}
